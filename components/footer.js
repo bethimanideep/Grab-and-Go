a7222d8a76a8d3795554089f0580706d98ecfb3e
arundaspr <arundaspr1999@gmail.com>
2023-01-20 21:27:04 +0530

    submitting day4

---
+++ components/footer.js
@@ -0,0 +1,74 @@
+let footer = () => {
+    return `
+<div id="footer">
+        <div class="container">
+            <div class="first-column">
+                <p id="column-heading">bigbasket</p>
+                <p><a href="" id="keys">About Us</a></p>
+                <p><a href="" id="keys">In News</a></p>
+                <p><a href="" id="keys">Green Bibbasket</a></p>
+                <p><a href="" id="keys">privacy Policy</a></p>
+                <p><a href="" id="keys">Affiliate</a></p>
+                <p><a href="" id="keys">Terms and Conditions</a></p>
+                <p><a href="" id="keys">Carrers At bigbasket</a></p>
+                <p><a href="" id="keys">bb Instantly</a></p>
+                <p><a href="" id="keys">bb Daily</a></p>
+                <p><a href="" id="keys">bb Blog</a></p>
+                <p><a href="" id="keys">bbnow</a></p>
+            </div>
+            <div class="first-column">
+                <p id="column-heading">Help</p>
+                <p><a href="" id="keys">FAQs</a></p>
+                <p><a href="" id="keys">Contact Us</a></p>
+                <p><a href="" id="keys">bb Wallet FAQs</a></p>
+                <p><a href="" id="keys">bb Wallet T&Cs</a></p>
+                <p><a href="" id="keys">Vendoe Connect</a></p>
+                
+            </div>
+            <div class="third-column first-column">
+                <p id="column-heading">Download Our App</p>
+                <a href="https://play.google.com/store/apps/details?id=com.bigbasket.mobileapp" target="blank"><img src="https://www.bbassets.com/static/v2499/custPage/build/content/img/Google-App-store-icon.png" alt=""></a>
+                <a href="https://apps.apple.com/in/app/bigbasket-com/id660683603?ign-mpt=uo%3D4"target="blank"><img src="https://www.bbassets.com/static/v2499/custPage/build/content/img/Apple-App-store-icon.png" alt=""></a>
+            </div>
+            <div class="fourth-column first-column">
+                <p id="column-heading">Get Soical With Us</p>
+                <a href="https://www.facebook.com/Bigbasketcom/" target="blank"><i class="fab fa-facebook"></i></a>
+                <a href="https://in.pinterest.com/bigbasketcom/"target="blank"><i class="fab fa-pinterest"></i></a>
+                <a href="https://twitter.com/Bigbasket_com"  target="blank"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/bigbasketcom/"target="blank"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+        <div class="bottom">
+            <div class="bottom-keys">
+                <p>POPULAR CATAGORIES:</p>
+                <p>POPULAR BRANDS:</p>
+                <p id="places">CITIES WE SERVE:</p>
+                <p id="payment">PAYMENT OPTIONS:</p>
+            </div>
+            <div class="values">
+                <p id="value-description"><a href="#">Sunflower Oils, Wheat Atta, Ghee, Milk, Health Drinks, Flakes, Organic F&V, Namkeen, Eggs, Floor Cleaner
+                    Other Juices, Leafy Vegetables, Frozen Veg Food, Dipers & Wipers,  
+                </a></p>
+                <p id="value-description"><a href="#">Amul, Nescafe, MTR, Red Bull, elite cake, Pediasure, Yummiez, yera, Yakult, Britania, Wow Momo, Fortune
+                    , Haldirams, Ferrero, Lays, patanjali, McCain, kwality walls, Cadbury Dairy Milk, Pedigree, 
+                </a></p>
+                <p id="value-description">Bangalore, Hyderabad, Mumbai, Pune, Chennai, Delhi, Mysore, Madurai, Coimbatore, Vijayawada-Guntur, Kolkata, Ahmedabad-Gandhinagar, Nashik Business, Lucknow-Kanpur, Gurgaon, Vadodara, Visakhapatnam, Surat, Nagpur, Patna, Indore, Chandigarh Tricity, Jaipur, Bhopal, Noida-Ghaziabad, Kochi, Krishna District, Bhubaneshwar-Cuttack, Guwahati, Renigunta, Hubli, Davanagere, Trichy, Amravati, Raipur, Rajkot, Gwalior, Bareilly, Allahabad, Hyderabad Rural, Bangalore Rural, Chennai Rural, Vizag Rural, Lucknow Rural, Noida Rural, Ahmedabad Rural, Bhopal Rural, Bhubaneswar Rural, Coimbatore Rural, Chandigarh Rural, Gurugram Rural, Guwahati Rural, Indore Rural, Kochi Rural, Kolkata Rural, Mumbai Rural, Mysore Rural, Nagpur Rural, Patna Rural, Pune Rural, Surat Rural, Vadodara Rural, Jaipur Rural, Ranchi, Nashik, Agra, Kozhikode </p>
+               <p id="pay-options">
+                <span> <span id="COD"><sup>CASH ON DELIVERY</sup></span></span>
+                <span><img src="/images/Visa.png" alt="visa_logo" id="paymentImg"></span>
+                <span><img src="/images/MasterCard.png" alt="MasterCard_logo" id="paymentImg"></span>
+                <span><img src="/images/Paytm.png" alt="paytm_logo" id="paymentImg"></span>
+                <span><img src="/images/mobikwik.png" alt="mobiwik_logo" id="paymentImg"></span>
+                <span><img src="/images/RuPay.png" alt="Rupay_logo" id="paymentImg1"></span>
+                <span><img src="/images/americanExpress.png" alt="americanExpress_logo" id="paymentImg2"></span>
+               </p>
+            </div>
+        </div>
+        <div class="CR">
+            <p>Copyright © 2022-2024 Supermarket Grocery Supplies Pvt Ltd</p>
+        </div>
+    </div>
+`
+}
+
+export { footer };
