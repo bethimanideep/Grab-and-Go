7365924b82eaed64372feddfaef1e1a613654a0e
Amit <pathakamit399@gmail.com>
2023-01-22 11:21:50 +0530

    day-5

---
+++ Address.js
@@ -0,0 +1,28 @@
+let nam=document.getElementById('name')
+let city=document.getElementById('city')
+let pin=document.getElementById('pin')
+let add=document.getElementById('add')
+let data=localStorage.getItem('address')||[]
+let total=localStorage.getItem('totalcost')
+let pr=document.getElementById("PR")
+let tr=document.getElementById("TR")
+let br=document.getElementById("BR")
+
+
+pr.innerText=total
+tr.innerText=total
+br.innerText=total
+
+
+
+
+
+
+
+
+
+document.querySelector("#button4").addEventListener("click", function () {
+  let val=`${nam.value},${city.value},${add.value},${pin.value}`
+  localStorage.setItem('address',val)
+  window.location.href = "Payment.html"
+});
