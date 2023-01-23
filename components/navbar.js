567ee2b3db44becd2baecb51c013510e68be1302
arundaspr <arundaspr1999@gmail.com>
2023-01-23 10:13:46 +0530

    submitting navcss

--- components/navbar.js
+++ components/navbar.js
@@ -37,7 +37,7 @@ let navbar = () => {
     <div class="Second-box">
         <div class="phone"><span><i class="fa-solid fa-phone"></i></span> 1860 123 1000</div>
         <div class="input-area"><input type="text" id="search" placeholder="Search for Products..."><button id="btn"><i class="fa-solid fa-magnifying-glass"></i></button></div>
-        <div class="offer"><span class="offers"><i class="fas fa-tag"></i> OFFERS</span><span class="empty"></span></div>
+        
     </div>
     <div class="third-box">
         <div class="bangalore"><i class="fas fa-map-marker-alt"></i> <span>560004, Bangalore <i class="fas fa-angle-down"></i></span> <i class="far fa-user"></i><a href="/routes/login.html" id ="login">${name}</a></div>
